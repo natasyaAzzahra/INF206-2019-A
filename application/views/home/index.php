b4f74e9cfec7015754033b1476a0ca6b14c372c7
Miftahul Jannah <miftahuljan537@gmail.com>
2019-04-25 22:13:22 +0700

    Mengubah Deskripsi Fitur Forum

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -194,14 +194,14 @@
             <div class="row align-items-center pb-5 pt-1">
                 <div class="col-lg-5 offset-1">
                     <h3 class="text-dark font-weight-light">Forum</h3>
-                    <p class="text-dark mt-4 text-justify">Lorem Ipsum is simply dummy text of the printing
-                        and typesetting industry.
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
-                        printer took a galley of type and scrambled it to make a type specimen book. It has survived not
-                        only five centuries, but also the leap into electronic typesetting, remaining essentially
-                        unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem
-                        Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker
-                        including versions of Lorem Ipsum
+                    <p class="text-dark mt-4 text-justify">Forum adalah sebuah sarana komunikasi bagi para penggunanya.
+                        Sebuah forum diskusi bisa menjadi salah satu cara yang dapat memperkaya pengalaman para nelayan.
+                        Forum sendiri dibuat agar para penggunanya menjadi lebih nyaman dalam berkomunikasi dengan nelayan lain.
+                        Forum merupakan wadah yang dapat digunakan oleh para nelayan untuk dapat berbagi pengalaman dengan nelayan lain
+                        dengan cara bertanya sekaligus menjawab pertanyaan yang masuk kedalam forum tersebut. 
+                        Forum sangatlah penting dikarenakan konsep dan mekanismenya sendiri lebih mengutamakan adanya proses diskusi antara
+                        anggota forum nelayan. Sehingga dengan adanya forum akan membuat metode diskusi menjadi lebih baik.
+
                     </p>
                     <a href="#" class="btn btn-dark mt-4">Read More</a>
                 </div>
