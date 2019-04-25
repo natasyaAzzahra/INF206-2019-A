2701a5e741a3d0aa03a99573053e4715f5f4e71e
Natasya Azzahra <achanata40@gmail.com>
2019-04-25 23:07:08 +0700

    menambah link foto untuk slide di fitur forum

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -205,7 +205,7 @@
                             </ol>
                             <div class="carousel-inner">
                                 <div class="carousel-item active">
-                                    <img class="d-block w-100" src="../../../assets/img/bg2.jpg" alt="First slide">
+                                    <img class="d-block w-100" src="https://i.imgur.com/yYWlL6i.jpg" alt="First slide">
                                 </div>
                                 <div class="carousel-item">
                                     <img class="d-block w-100" src="../../../assets/img/bg3.jpg" alt="Second slide">
