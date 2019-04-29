26ab7a3c81eaf2ce4c67f91b5f3da05d89753b33
putra <putra.maghfirah@gmail.com>
2019-04-29 22:43:57 +0700

    memisahkan dan menambahkan header footer

---
+++ application/views/templates/header.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<title>Nelayan Belajar</title>
+	<!--     Fonts and icons     -->
+	<link rel="stylesheet" type="text/css"
+		href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+		integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+	<!-- Material Kit CSS -->
+	<link href="assets/css/material-kit.css?v=2.0.5" rel="stylesheet" />
+	<link rel="stylesheet" href="style.css">
+	<style>
+		.place {
+			color: white;
+		}
+
+		#tombol {
+			margin-left: 10px;
+			border-radius: 100%;
+			height: 40px;
+			width: 40px;
+		}
+
+	</style>
+</head>
+
+<body>	
+    <!-- navbar -->
+	<nav class="navbar navbar-transparent bg-dark navbar-color-on-scroll fixed-top navbar-expand-lg"
+		color-on-scroll="100">
+		<div class=" container">
+			<div class="navbar-translate">
+				<a class="navbar-brand" href="http://localhost/INF206-2019-A/">Nelayan Belajar</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="navbar-toggler-icon"></span>
+					<span class="navbar-toggler-icon"></span>
+					<span class="navbar-toggler-icon"></span>
+				</button>
+			</div>
+
+			<div class="collapse navbar-collapse">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a href="#" class="nav-link">Wawasan</a>
+					</li>
+					<li class="nav-item">
+						<a href="#" class="nav-link">Peraturan UU</a>
+					</li>
+					<li class="nav-item">
+						<a href="#" class="nav-link">Forum</a>
+					</li>
+				</ul>
+
+				<form class="form-inline ml-auto nav-item navbar-nav">
+					<div class="form-group no-border pr-4">
+						<input type="text" class="form-control place" placeholder="Search" style="color: grey;">
+					</div>
+					<!-- <button type="submit" class="btn bg-transparent btn-just-icon btn-round" style="color: grey;">
+                        <i class="material-icons">search</i>
+                    </button> -->
+				</form>
+
+				<div class="btn-group nav-item navbar-nav">
+					<a href="http://localhost/INF206-2019-A/assets/akun/login.php">
+						<button class="btn bg-transparent btn-round" style="border:thick; color: grey;">Masuk</button>
+					</a>
+				</div>
+			</div>
+		</div>
+	</nav>
+    <!-- end of navbar -->
+
+    <!-- textarea -->
+	<div class="page-header header-filter" data-parallax="true"
+		style="background-image: url('https://i.imgur.com/b8LbQU3.jpg')">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 ml-auto mr-auto">
+					<div class="brand text-center">
+						<h1>Nelayan Belajar</h1>
+						<h3 class="title text-center">"Temukan Solusi Masalah Aktivitas Nelayan Disini"</h3>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+    <!-- end of textarea -->
+    
+    	<!-- Content -->
+	<div class="main main-raised">
+		<div class="container menu">
