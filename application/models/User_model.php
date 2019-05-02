8d65d24143d58b8cb0434840b574e0ca00fa0538
putra <putra.maghfirah@gmail.com>
2019-05-02 22:22:02 +0700

    fix

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 class User_model extends CI_Model
 {
@@ -14,20 +13,3 @@ class User_model extends CI_Model
         $this->db->insert('user', $data);
     }
 }
-=======
-<?php
-class User_model extends CI_Model
-{
-    public function tambahDataUser()
-    {
-        $data = array(
-            "namalengkap" => $this->input->post('namalengkap', true),
-            "username" => $this->input->post('username', true),
-            "email" => $this->input->post('email', true),
-            "password" => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
-            "role" => 2
-        );
-        $this->db->insert('user', $data);
-    }
-}
->>>>>>> 6ebfb919656230060a2d76d521fb9c417b2705b4
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!-- Footer -->
 <footer class="footer">
 	<div class="container">
@@ -60,69 +59,22 @@
 <!-- Control Center for Material Kit: parallax effects, scripts for the example pages etc -->
 <script src="<?= base_url(); ?>assets/js/material-kit.js?v=2.0.5" type="text/javascript"></script>
 </body>
-=======
-    <!-- Footer -->
-    <footer class="footer">
-    	<div class="container">
-    		<div class="row">
-    			<div class="col-md-4">
-    				<h1 class="title">Nelayan Belajar</h1>
-    			</div>
 
-    			<div class="col-md-4 pt-5 pl-5">
-    				<ul class="nav">
-    					<li class="nav-item ">
-    						<a href="#" class="nav-link">
-    							Contact Us
-    						</a>
-    					</li>
-    					<li class="nav-item">
-    						<a href="#" class="nav-link">
-    							About Us
-    						</a>
-    					</li>
-    					<li class="nav-item">
-    						<a href="#" class="nav-link">
-    							License
-    						</a>
-    					</li>
-    				</ul>
-    			</div>
-    			<div class="col-md-4">
-    				<h3 class="title">Follow us:</h3>
-    				<div class="btn-wrapper profile">
-    					<a target="_blank" href="#" class="btn btn-icon btn-neutral btn-round btn-simple" data-toggle="tooltip" data-original-title="Follow us">
-    						<i class="fab fa-twitter"></i>
-    					</a>
-    					<a target="_blank" href="#" class="btn btn-icon btn-neutral btn-round btn-simple" data-toggle="tooltip" data-original-title="Like us">
-    						<i class="fab fa-facebook-square"></i>
-    					</a>
-    					<a target="_blank" href="#" class="btn btn-icon btn-neutral  btn-round btn-simple" data-toggle="tooltip" data-original-title="Follow us">
-    						<i class="fab fa-dribbble"></i>
-    					</a>
-    				</div>
-    			</div>
-    		</div>
-    	</div>
-    </footer>
-    <!-- end of footer -->
->>>>>>> 6ebfb919656230060a2d76d521fb9c417b2705b4
+<!--   Core JS Files   -->
+<script src="<?= base_url(); ?>assets/js/core/jquery.min.js" type="text/javascript"></script>
+<script src="<?= base_url(); ?>assets/js/core/popper.min.js" type="text/javascript"></script>
+<script src="<?= base_url(); ?>assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
+<script src="<?= base_url(); ?>assets/js/plugins/moment.min.js"></script>
+<!--	Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
+<script src="<?= base_url(); ?>assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
+<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+<script src="<?= base_url(); ?>assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+<!--  Google Maps Plugin  -->
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+<!-- Place this tag in your head or just before your close body tag. -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+<!-- Control Center for Material Kit: parallax effects, scripts for the example pages etc -->
+<script src="<?= base_url(); ?>assets/js/material-kit.js?v=2.0.5" type="text/javascript"></script>
+</body>
 
-    <!--   Core JS Files   -->
-    <script src="<?= base_url(); ?>assets/js/core/jquery.min.js" type="text/javascript"></script>
-    <script src="<?= base_url(); ?>assets/js/core/popper.min.js" type="text/javascript"></script>
-    <script src="<?= base_url(); ?>assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
-    <script src="<?= base_url(); ?>assets/js/plugins/moment.min.js"></script>
-    <!--	Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
-    <script src="<?= base_url(); ?>assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
-    <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-    <script src="<?= base_url(); ?>assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
-    <!--  Google Maps Plugin  -->
-    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
-    <!-- Place this tag in your head or just before your close body tag. -->
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
-    <!-- Control Center for Material Kit: parallax effects, scripts for the example pages etc -->
-    <script src="<?= base_url(); ?>assets/js/material-kit.js?v=2.0.5" type="text/javascript"></script>
-    </body>
-
-    </html>
+</html>
