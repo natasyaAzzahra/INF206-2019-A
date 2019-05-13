a1c091163ce8ce98c3ed132fd6c0df2801b5fb52
putra <putra.maghfirah@gmail.com>
2019-05-13 16:01:03 +0700

    menambahkan migrasi database

---
+++ application/migrations/001_user.php
@@ -0,0 +1,55 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Migration_user extends CI_Migration
+{
+
+    public function __construct()
+    {
+        $this->load->dbforge();
+        $this->load->database();
+    }
+    public function up()
+    {
+        $this->dbforge->add_field(array(
+            'id' => array(
+                'type' => 'INT',
+                'constraint' => 20,
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE
+            ),
+            'namalengkap' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ),
+            'username' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ),
+            'email' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ),
+            'password' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ),
+            'role_id' => array(
+                'type' => 'INT',
+                'constraint' => '11',
+            ),
+            'image' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '128',
+            ),
+        ));
+        $this->dbforge->add_key('id', TRUE);
+        $this->dbforge->create_table('user');
+    }
+
+    public function down()
+    {
+        $this->dbforge->drop_table('user');
+    }
+}
