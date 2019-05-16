e80122f1e243bbb6c5821908a1e3a7c8d0afc900
putra <putra.maghfirah@gmail.com>
2019-05-16 17:34:34 +0700

    memperbaiki bug dan mengubah sedikit halaman

--- application/views/fitur/viewtentangkami.php
+++ application/views/fitur/viewtentangkami.php
@@ -1,78 +1,66 @@
-        <!-- textarea -->
-        <div class="page-header header-filter" data-parallax="true" style="background-image: url('https://i.imgur.com/b8LbQU3.jpg')">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8 ml-auto mr-auto">
-                        <div class="brand text-center">
-                            <h1><?= $judul ?></h1>
-                            <h3 class="title text-center"><?= $isi ?></h3>
+<body class="profile-page">
+    <div class="page-header header-filter" data-parallax="true" style="background-image: url('https://i.imgur.com/8SHVwNO.jpg')"></div>
+    <div class="main main-raised">
+        <!-- konten -->
+        <div class="container">
+            <h2 class="fitur pt-5 text-center"><span>Tentang Kami</span></h2>
+            <div class="row justify-content-center">
+                <div class="col-md-3 text-center">
+                    <div class="card" style="width: 18rem;">
+                        <img src="<?= base_url('assets/img/default.jpg'); ?>" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">Putra Maghfirah</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-3 mr-5 text-center">
+                    <div class="card" style="width: 18rem;">
+                        <img src="<?= base_url('assets/img/default.jpg'); ?>" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">Natasya Azzahra</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 ml-5 text-center">
+                    <div class="card" style="width: 18rem;">
+                        <img src="<?= base_url('assets/img/default.jpg'); ?>" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">Rezky Yuniati</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-3 mr-5 text-center">
+                    <div class="card" style="width: 18rem;">
+                        <img src="<?= base_url('assets/img/default.jpg'); ?>" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">Mauliza Ananda</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 ml-5 text-center">
+                    <div class="card" style="width: 18rem;">
+                        <img src="<?= base_url('assets/img/default.jpg'); ?>" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">Miftahul Jannah</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- end of textarea -->
-
-        <!-- Content -->
-        <div class="main main-raised">
-            <div class="container">
-                <h2 class="fitur pt-5 text-center"><span>Tentang Kami</span></h2>
-                <div class="row justify-content-center">
-                    <div class="col-md-3 text-center">
-                        <div class="card" style="width: 18rem;">
-                            <img src="<?= base_url('assets/img/default.jpg'); ?>" class="card-img-top" alt="">
-                            <div class="card-body">
-                                <h5 class="card-title">Putra Maghfirah</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row justify-content-center">
-                    <div class="col-md-3 mr-5 text-center">
-                        <div class="card" style="width: 18rem;">
-                            <img src="<?= base_url('assets/img/default.jpg'); ?>" class="card-img-top" alt="">
-                            <div class="card-body">
-                                <h5 class="card-title">Natasya Azzahra</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3 ml-5 text-center">
-                        <div class="card" style="width: 18rem;">
-                            <img src="<?= base_url('assets/img/default.jpg'); ?>" class="card-img-top" alt="">
-                            <div class="card-body">
-                                <h5 class="card-title">Rezky Yuniati</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row justify-content-center">
-                    <div class="col-md-3 mr-5 text-center">
-                        <div class="card" style="width: 18rem;">
-                            <img src="<?= base_url('assets/img/default.jpg'); ?>" class="card-img-top" alt="">
-                            <div class="card-body">
-                                <h5 class="card-title">Mauliza Ananda</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3 ml-5 text-center">
-                        <div class="card" style="width: 18rem;">
-                            <img src="<?= base_url('assets/img/default.jpg'); ?>" class="card-img-top" alt="">
-                            <div class="card-body">
-                                <h5 class="card-title">Miftahul Jannah</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- end of Content -->
+        <!-- end of konten -->
+    </div>
+</body>
